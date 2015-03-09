6a0a50bb17d9b55a0681f6922be8af9ca1b7c922
Danny Weng <danny.gl.weng@gmail.com>
2015-03-09 01:25:06 -1000

    Clean version

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,20 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
 
-import views.html.*;
-
+/**
+ * Controller for application.
+ */
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
+  /**
+   * Controller for main page.
+   * @return Rendered results.
+   */
+  public static Result index() {
+    return ok(index.render("Your new application is ready."));
+  }
 
 }
---
+++ app/controllers/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by Weng on 3/9/2015.
+ */
+package controllers;
